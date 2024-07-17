42af2920c3a661db4b380769abd390d66846e3b8
Sherzod <shokirovsherzod5@gmail.com>
2024-07-17 18:35:52 +0500

    df

--- config/settings.py
+++ config/settings.py
@@ -68,10 +68,22 @@ CSRF_TRUSTED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tashmediatrans',
+        'USER': 'postgres',
+        'PASSWORD': 'eCbJQbYqs1223oB',
+        'HOST': 'postgres',
+        # 'HOST': '91.205.105.27',
+        'PORT': '5432',
     }
 }
 
